6ce15155877bd80ebad81a3f2cebc95aeaaafc32
Dennis Jaamann <dennis.jaamann@ordina.be>
2014-11-10 21:36:31 +0100

    Added contact page + route

--- solution/app/scripts/app.js
+++ solution/app/scripts/app.js
@@ -24,6 +24,10 @@
           templateUrl: 'views/about.html',
           controller: 'AboutCtrl'
         })
+        .when('/contact',{
+          templateUrl: 'views/contact.html',
+          controller: 'ContactCtrl'
+        })
         .otherwise({
           redirectTo: '/'
         });
