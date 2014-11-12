ee1942ffb9cdf57b7cc0775233b6bbbdc6b9f2d8
Dennis Jaamann <dennis.jaamann@ordina.be>
2014-11-12 22:15:51 +0100

    Fixed data

--- solution/app/scripts/about/services/aboutService.js
+++ solution/app/scripts/about/services/aboutService.js
@@ -12,7 +12,7 @@
     .service('AboutService', function aboutService($q, $log) {
       // AngularJS will instantiate a singleton by calling "new" on this function
       var title = 'Some about stuff';
-      var content = 'Wow such stuff, much stuffness';
+      var content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
       this.fetchAbout = function () {
         $log.log('Fetching about');
