ae05a5273051d3b23057b4478abcf6f978b49c3a
Dennis Jaamann <dennis.jaamann@ordina.be>
2014-11-12 20:05:32 +0100

    Added contactservice test

---
+++ solution/test/spec/contact/service/contactServic.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Service: ContactService', function () {
+
+  // load the service's module
+  beforeEach(module('solutionApp'));
+  beforeEach(module('contact'));
+
+  // instantiate service
+  var ContactService;
+  beforeEach(inject(function (_ContactService_) {
+    ContactService = _ContactService_;
+  }));
+
+  it('should do something', function () {
+    expect(!!ContactService).toBe(true);
+  });
+
+});
